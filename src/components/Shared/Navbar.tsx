f783b61782a65d0ff1225f17e93fdb5997802457
Mominul <ulhouqemomin39@gmail.com>
2024-12-16 14:26:20 +0600

    first commit

---
+++ src/components/Shared/Navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <div>
+      <ul className="flex justify-between ">
+        <li>
+          <a href="">Logo</a>
+        </li>
+        <li>
+          <a href="">Login</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/UI/Homepage.tsx
@@ -0,0 +1,5 @@
+const Homepage = () => {
+  return <div>This is home page</div>;
+};
+
+export default Homepage;
